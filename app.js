b45bd0a62b0c7303960769b7e712d5db1f891fa0
sarahTarablus <sarahtarablus@gmail.com>
2020-12-22 12:26:04 -0800

    still not loading

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 function loadImage(){
   const img = new Image();
-  img.src = 'icons/rock.png';
+  img.src = './rock.png';
   img.onload = () => {
   ctx.drawImage(img, 0, 0)
   }
 }
+
+loadImage();
